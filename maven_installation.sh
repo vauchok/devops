a2c493e07b79c4157f1ae527d22de50e145c299a
vauchok <napa93@mail.ru>
2018-01-17 13:36:07 +0000

    Add maven

---
+++ maven_installation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Installation and configuration Maven-3.5.2
+
+cd /opt/
+wget http://www-us.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
+tar xzf apache-maven-3.5.2-bin.tar.gz 
+sed -i '/PATH=$PATH:$HOME/a \ export M2_HOME=/opt/apache-maven-3.5.2' ~/.bashrc 
+sed -i '/export M2_HOME/a \ export PATH=$M2_HOME/bin:$PATH' ~/.bashrc
+source ~/.bashrc
