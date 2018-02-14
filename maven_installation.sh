77580f71009a7dcd301aaf3f487e3e771d1ecd87
Ihar Vauchok <napa93@mail.ru>
2018-02-14 13:42:39 +0300

    Changes4/maven_installation.sh

--- maven_installation.sh
+++ maven_installation.sh
@@ -7,3 +7,4 @@ tar xzf apache-maven-3.5.2-bin.tar.gz && rm -rf apache-maven-3.5.2-bin.tar.gz
 sed -i '/PATH=$PATH:$HOME/a \ export M2_HOME=/tmp/apache-maven-3.5.2' ~/.bashrc 
 sed -i '/export M2_HOME/a \ export PATH=$M2_HOME/bin:$PATH' ~/.bashrc
 source ~/.bashrc
+mvn -v
