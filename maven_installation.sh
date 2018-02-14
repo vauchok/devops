6e6feed88cda6a7e0d14d424b50a55dc5627277a
Ihar Vauchok <napa93@mail.ru>
2018-02-14 13:30:13 +0300

    Changes/maven_installation.sh

--- maven_installation.sh
+++ maven_installation.sh
@@ -2,8 +2,8 @@
 #Installation and configuration Maven-3.5.2
 
 cd /opt/
-wget http://www-us.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
-tar xzf apache-maven-3.5.2-bin.tar.gz && rm -rf apache-maven-3.5.2-bin.tar.gz 
+sudo wget http://www-us.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
+sudo tar xzf apache-maven-3.5.2-bin.tar.gz && sudo  rm -rf apache-maven-3.5.2-bin.tar.gz 
 sed -i '/PATH=$PATH:$HOME/a \ export M2_HOME=/opt/apache-maven-3.5.2' ~/.bashrc 
 sed -i '/export M2_HOME/a \ export PATH=$M2_HOME/bin:$PATH' ~/.bashrc
 source ~/.bashrc
