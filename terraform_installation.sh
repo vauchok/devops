b0cbdac399ce4f1ddd8e4dff93b8b94df2fdcebc
Ihar Vauchok <31825372+vauchok@users.noreply.github.com>
2018-04-19 15:49:48 +0300

    Add rm terraform_0.11.7_linux_amd64.zip

--- terraform_installation.sh
+++ terraform_installation.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+ $$#!/bin/bash
 
 yum -y install unzip
 wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
-unzip terraform_0.11.7_linux_amd64.zip -d /usr/local/bin/
+unzip terraform_0.11.7_linux_amd64.zip -d /usr/local/bin/ && rm -rf terraform_0.11.7_linux_amd64.zip
 ln -s /usr/local/bin/terraform /usr/bin/terraform
 terraform -v
