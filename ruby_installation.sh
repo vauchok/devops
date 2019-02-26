9045a23a393ddcc9a39cf10de35595b8e9aa98b4
Ihar Vauchok <31825372+vauchok@users.noreply.github.com>
2019-02-26 16:02:24 +0300

    fix

--- ruby_installation.sh
+++ ruby_installation.sh
@@ -4,7 +4,6 @@ yum install -y gcc-c++ patch readline readline-devel zlib zlib-devel \
    libyaml-devel libffi-devel openssl-devel make \
    bzip2 autoconf automake libtool bison iconv-devel sqlite-devel
 
-curl -sSL https://rvm.io/mpapis.asc | gpg --import -
 gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 curl -L get.rvm.io | bash -s stable
 
